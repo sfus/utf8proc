fcce19be867ad6c901c3d26f0155cc3e2246612f
atton <e115763@gmail.com>
2017-03-20 11:30:48 +0900

    Set magic comment

--- data/data_generator.rb
+++ data/data_generator.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 #  This file was used to generate the 'unicode_data.c' file by parsing the
 #  Unicode data file 'UnicodeData.txt' of the Unicode Character Database.
